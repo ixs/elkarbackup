74b1bf80b9574ccba6dd5e5ced5b6a24f98b25c5
Andreas Thienemann <ixs@github.com>
2025-07-05 18:06:41 +0200

    Also tag docker images with the git shortcommit

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -7,4 +7,6 @@ export DOCKER_BUILDKIT=1
 PLATFORM=linux/amd64,linux/arm64
 PLATFORM=linux/amd64
 
-(cd $DIR/..; docker buildx build --platform $PLATFORM --push -t ghcr.io/ixs/elkarbackup:latest -f docker/Dockerfile .)
+COMMIT_ID=$(git rev-parse --short HEAD)
+
+(cd $DIR/..; docker buildx build --platform $PLATFORM --push -t ghcr.io/ixs/elkarbackup:latest -t ghcr.io/ixs/elkarbackup:$COMMIT_ID -f docker/Dockerfile .)
