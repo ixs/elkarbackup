8ced1b9d70352856ef2f399f3a834aee9fdfeb67
Andreas Thienemann <ixs@github.com>
2025-07-06 21:29:26 +0200

    Nicer grouping

--- run-tests.sh
+++ run-tests.sh
@@ -3,10 +3,18 @@
 export APP_ENV=test
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "::group::Database setup"
 $DIR/bin/console doctrine:database:drop --force
 $DIR/bin/console doctrine:database:create
 $DIR/bin/console doctrine:migrations:migrate --no-interaction
+echo "::endgroup::"
+
+echo "::group::General setup"
 sudo --preserve-env $DIR/bin/console elkarbackup:create_admin 
 mkdir -p /tmp/elkarbackup-tests/uploads
 $DIR/bin/console hautelook:fixtures:load --append
+echo "::endgroup::"
+
+echo "::group::Running unit tests"
 $DIR/bin/phpunit "${@:1}"
+echo "::endgroup::"
