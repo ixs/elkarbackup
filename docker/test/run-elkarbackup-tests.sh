272d6f2a59ebf79c02ec55a26ca67b567a6896d8
Andreas Thienemann <ixs@github.com>
2025-07-06 20:03:02 +0200

    Get tests running in docker. Drop special composer.json, use the regular one. Fix the phpunit version for symfony to be 8.5.34 for now.

--- docker/test/run-elkarbackup-tests.sh
+++ docker/test/run-elkarbackup-tests.sh
@@ -3,3 +3,14 @@
 DIR="$(dirname "$(readlink -f "$0")")"
 
 docker-compose -f "${DIR}/docker-compose.yml" up --abort-on-container-exit --exit-code-from elkarbackup
+err=$?
+
+# Cleanup
+docker compose -f "${DIR}/docker-compose.yml" down
+
+if [ "$err" -eq 0 ]; then
+    echo -e "\033[1;32m✅ Success: All checks passed!\033[0m"
+else
+    echo -e "\033[1;31m❌ Failure: One or more checks failed (err=$err).\033[0m"
+fi
+exit $err
